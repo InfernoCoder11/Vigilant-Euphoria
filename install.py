d1c0505bd0cc47c51b2e8d8e4807f384bb66c064
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2018-06-01 20:38:44 +0530

    Install file

---
+++ install.py
@@ -0,0 +1,31 @@
+import subprocess as sub
+import sys, tty
+import termios
+
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(fd)
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+print ("Checking if PyQt5 installed")
+Pyqt5Installed = False
+try:
+    import PyQt5
+    Pyqt5Installed = True
+    print ("PyQt5 installed")
+
+except ImportError:
+    print ("PyQt5 is not installed. Install it ?(y/n)")
+    ans = getch()
+
+    if ans == "y":
+        print ("\nInstalling PyQt5.....\n")
+        sub.call ("sudo apt-get install python3-pyqt5".split())
+    else:
+        print ("Exiting...")
+        sys.exit()
