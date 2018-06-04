ff874f1f3d17133b4e01909831d0452d3a6cda89
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2018-06-04 18:18:28 +0530

    Added print parantheses

--- install.py
+++ install.py
@@ -1,6 +1,7 @@
 import subprocess as sub
 import sys, tty
 import termios
+import os
 
 def getch():
     fd = sys.stdin.fileno()
@@ -12,6 +13,8 @@ def getch():
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
 
+InstallpyDir = os.path.dirname(os.path.realpath(__file__))
+
 print ("Running sudo apt-get update")
 sub.call ("sudo apt-get update".split())
 
@@ -19,26 +22,26 @@ print ("Running sudo apt-get upgrade")
 sub.call ("sudo apt-get upgrade".split())
 
 print ("Installing pip")
-sub.call ("sudo apt-get install python3-pip")
+sub.call ("sudo apt-get install python3-pip".split())
 
-print "\nInstalling virtualenv\n"
+print ("\nInstalling virtualenv\n")
 sub.call("sudo pip3 install virtualenv".split())
 
-print "\nChanging Directory to home\n"
-sub.call("cd".split())
+print ("\nChanging Directory to home\n")
+sub.call("cd $HOME".split())
 
-print "\nMaking Vigilant-Euphoria-Server directory\n"
+print ("\nMaking Vigilant-Euphoria-Server directory\n")
 sub.call("mkdir Vigilant-Euphoria-Server".split())
 
-print "\nChanging dir to Vigilant-Euphoria-Server\n"
+print ("\nChanging dir to Vigilant-Euphoria-Server\n")
 sub.call("cd Vigilant-Euphoria-Server".split())
 
-print "\nCreating virtual environment\n"
+print ("\nCreating virtual environment\n")
 sub.call ("virtualenv flask-env".split())
 
-print "\nActivating virtual environment\n"
+print ("\nActivating virtual environment\n")
 sub.call ("source flask-env/bin/activate".split())
 
-print "\nInstalling flask\n"
+print ("\nInstalling flask\n")
 sub.call ("pip install Flask".split())
 
