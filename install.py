393550f5d866c9ebe7f7ca8e09df98b9df7813bc
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2018-06-04 18:13:53 +0530

    Added all install.sh commands

--- install.py
+++ install.py
@@ -1,7 +1,6 @@
 import subprocess as sub
 import sys, tty
 import termios
-import time
 
 def getch():
     fd = sys.stdin.fileno()
@@ -12,3 +11,34 @@ def getch():
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
+
+print ("Running sudo apt-get update")
+sub.call ("sudo apt-get update".split())
+
+print ("Running sudo apt-get upgrade")
+sub.call ("sudo apt-get upgrade".split())
+
+print ("Installing pip")
+sub.call ("sudo apt-get install python3-pip")
+
+print "\nInstalling virtualenv\n"
+sub.call("sudo pip3 install virtualenv".split())
+
+print "\nChanging Directory to home\n"
+sub.call("cd".split())
+
+print "\nMaking Vigilant-Euphoria-Server directory\n"
+sub.call("mkdir Vigilant-Euphoria-Server".split())
+
+print "\nChanging dir to Vigilant-Euphoria-Server\n"
+sub.call("cd Vigilant-Euphoria-Server".split())
+
+print "\nCreating virtual environment\n"
+sub.call ("virtualenv flask-env".split())
+
+print "\nActivating virtual environment\n"
+sub.call ("source flask-env/bin/activate".split())
+
+print "\nInstalling flask\n"
+sub.call ("pip install Flask".split())
+
