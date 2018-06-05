421d64951b0403094a56cf511db21c813f312406
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2018-06-05 14:47:57 +0530

    Added notify-send action

--- Files/__init__.py
+++ Files/__init__.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import subprocess as sub
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-	return "Hello World!"
+def main():
+	return render_template("main.html")
+
+@app.route("/handle_data", methods = ["POST"])
+def handle_data():
+	heading = request.form["heading"]
+	message = request.form["message"]
+	sub.call (["notify-send", heading, message])
+	return render_template("main.html")
 
 if (__name__ == "__main__"):
 	app.run(port = 5567)
